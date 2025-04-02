6d1ead46de8d0e17959445e4db98b7706a1a7928
Arhway_Larhuna <122046228+Arhway2004@users.noreply.github.com>
2025-04-02 15:54:02 +0700

    login

--- monitor/src/app/(Pages)/login/page.tsx
+++ monitor/src/app/(Pages)/login/page.tsx
@@ -1,7 +1,9 @@
-export default function Login(){
+export default function Login() {
     return (
-        <div className="blue w-10 h-10">
-
+        <div className="relative w-screen h-screen">
+            <div className="bg-blue-500 w-10 h-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+             <h1>cdjsofcj</h1>
+            </div>
         </div>
-    )
+    );
 }
